3b6ea4768cad140e0011becbe7610972d343b06e
Anant Kumar <coderanant@gmail.com>
2021-02-14 17:39:21 +0530

    Add GET /memes/:id route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('./config/config');
 
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Meme } = require('./models/meme');
@@ -19,29 +21,51 @@ app.post('/memes', (req, res) => {
     });
 
     meme.save().then((doc) => {
-        res.send({'id': doc._id});
+        res.send({ 'id': doc._id });
     }).catch((e) => {
         res.status(400).send(e);
     });
 });
 
 app.get('/memes', (req, res) => {
-    Meme.find().sort({_id:-1}).limit(100).then((doc) => {
+    Meme.find().sort({ _id: -1 }).limit(100).then((doc) => {
         var memes = [];
         doc.forEach((i) => {
-            console.log(i);
-            var meme = {
+            memes.push({
                 'id': i._id,
                 'name': i.name,
                 'url': i.name,
                 'caption': i.caption
-            };
-            // console.log(meme);
-            memes.push(meme);
+            });
         });
         res.send(memes);
     }).catch((e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/memes/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send();
+    }
+
+    Meme.findOne({
+        _id: id
+    }).then((meme) => {
+        if (!meme) {
+            return res.status(404).send();
+        }
+
+        res.send({
+            id: meme._id,
+            name: meme.name,
+            url: meme.url,
+            caption: meme.caption
+        });
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
